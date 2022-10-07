1940a656ecfcd6d2722b813d4512e985eab98d43
Luis Gustavo <gusouza980@gmail.com>
2022-10-07 18:03:16 -0300

    Update

--- resources/views/exports/associados.blade.php
+++ resources/views/exports/associados.blade.php
@@ -13,19 +13,43 @@
             <td></td>
         </tr>
         <tr>
+            {{-- <td>FOTO</td> --}}
             <td>SEQ.</td>
             <td>NOME</td>
+            <td>MODALIDADE</td>
             <td>TITULO</td>
             <td>CPF</td>
             <td>RNP</td>
+            <td>NASCIMENTO</td>
+            <td>EMAIL</td>
+            <td>TELEFONE</td>
+            <td>CONSELHO</td>
+            <td>ENDERECO</td>
+            <td>CIDADE</td>
+            <td>UF</td>
+            <td>CEP</td>
+            <td>PAIS</td>
+            <td>SITUAÇÃO</td>
         </tr>
         @foreach($associados as $associado)
             <tr>
+                {{-- <td><img src="{{ $associado->foto }}" alt=""></td> --}}
                 <td>{{ $associado->id }}</td>
                 <td>{{ $associado->nome }}</td>
-                <td>@if($associado->titulo_profissional) {{ config("associados.titulis")[$associado->titulo_profissional] }} @endif</td>
+                <td>@if($associado->modalidade !== null) {{ config("associados.modalidades")[$associado->modalidade] }} @endif</td>
+                <td>@if($associado->titulo_profissional !== null) {{ config("associados.titulis")[$associado->titulo_profissional] }} @endif</td>
                 <td>{{ $associado->cpf }}</td>
                 <td>{{ $associado->registro_profissional }}</td>
+                <td>{{ date("d/m/Y", strtotime($associado->nascimento)) }}</td>
+                <td>{{ $associado->email }}</td>
+                <td>{{ $associado->telefone }}</td>
+                <td>{{ config("associados.conselhos")[$associado->conselho_profissional] }}</td>
+                <td>{{ $associado->endereco_atendimento }}</td>
+                <td>{{ $associado->cidade_atendimento }}</td>
+                <td>{{ $associado->uf_atendimento }}</td>
+                <td>{{ $associado->cep_atendimento }}</td>
+                <td>{{ $associado->pais_atendimento }}</td>
+                <td>{{ config("associados.situacoes")[$associado->situacao] }}</td>
             </tr>
         @endforeach
     </tbody>
