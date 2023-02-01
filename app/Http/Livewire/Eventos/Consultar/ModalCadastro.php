e115d73d83a149b4748ad2879a8c075f3d656732
Everaldo Júnior <everaldocrj@gmail.com>
2023-02-01 17:01:53 -0300

    🎃 [ UF ] - [ Correção Palestrante ]

--- app/Http/Livewire/Eventos/Consultar/ModalCadastro.php
+++ app/Http/Livewire/Eventos/Consultar/ModalCadastro.php
@@ -8,51 +8,54 @@ use App\Classes\Util;
 
 class ModalCadastro extends Component
 {
-    public $evento;
+	public $evento;
 
-    protected $listeners = ["carregaModalCadastroEvento", "carregaModalEdicaoEvento", "resetaModalCadastroEvento"];
-    protected $rules = [
-        "evento.titulo" => "",
-        "evento.data" => "",
-        "evento.horario" => "",
-        "evento.local" => "",
-        "evento.tema" => "",
-        "evento.palestrante" => "",
-        "evento.conteudo" => "",
-        "evento.data" => "",
-        "evento.horario" => "",
-        "evento.horario_fim" => "",
-        "evento.utiliza_palestras" => ""
-    ];
+	protected $listeners = ["carregaModalCadastroEvento", "carregaModalEdicaoEvento", "resetaModalCadastroEvento"];
+	protected $rules = [
+		"evento.titulo" => "",
+		"evento.data" => "",
+		"evento.horario" => "",
+		"evento.local" => "",
+		"evento.tema" => "",
+		"evento.conteudo" => "",
+		"evento.data" => "",
+		"evento.horario" => "",
+		"evento.horario_fim" => "",
+		"evento.utiliza_palestras" => ""
+	];
 
-    public function carregaModalCadastroEvento(){
-        $this->evento = new Evento;
-        $this->dispatchBrowserEvent("abreModalCadastroEvento");
-    }
+	public function carregaModalCadastroEvento()
+	{
+		$this->evento = new Evento;
+		$this->dispatchBrowserEvent("abreModalCadastroEvento");
+	}
 
-    public function carregaModalEdicaoEvento($id){
-        $this->evento = Evento::find($id);
-        $this->dispatchBrowserEvent("carregaTexto");
-        $this->dispatchBrowserEvent("abreModalCadastroEvento");
-    }
+	public function carregaModalEdicaoEvento($id)
+	{
+		$this->evento = Evento::find($id);
+		$this->dispatchBrowserEvent("carregaTexto");
+		$this->dispatchBrowserEvent("abreModalCadastroEvento");
+	}
 
-    public function resetaModalCadastroEvento(){
-        $this->reset();
-    }
+	public function resetaModalCadastroEvento()
+	{
+		$this->reset();
+	}
 
-    public function salvar(){
-        $this->evento->conteudo = Util::processa_editor($this->evento->id, $this->evento->conteudo, 'imagems/eventos/');
-        if($this->evento->utiliza_palestras === null){
-            $this->evento->utiliza_palestras = 0;
-        }
-        $this->evento->save();
-        $this->dispatchBrowserEvent('notificaToastr', ['tipo' => 'success', 'mensagem' => 'Informações salvas com sucesso!']);
-        $this->emit("atualizaDatatableEventos");
-        $this->dispatchBrowserEvent("fechaModalCadastroEvento");
-    }
+	public function salvar()
+	{
+		$this->evento->conteudo = Util::processa_editor($this->evento->id, $this->evento->conteudo, 'imagems/eventos/');
+		if ($this->evento->utiliza_palestras === null) {
+			$this->evento->utiliza_palestras = 0;
+		}
+		$this->evento->save();
+		$this->dispatchBrowserEvent('notificaToastr', ['tipo' => 'success', 'mensagem' => 'Informações salvas com sucesso!']);
+		$this->emit("atualizaDatatableEventos");
+		$this->dispatchBrowserEvent("fechaModalCadastroEvento");
+	}
 
-    public function render()
-    {
-        return view('livewire.eventos.consultar.modal-cadastro');
-    }
+	public function render()
+	{
+		return view('livewire.eventos.consultar.modal-cadastro');
+	}
 }
