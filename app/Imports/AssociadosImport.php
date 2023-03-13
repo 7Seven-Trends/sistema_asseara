3f2d65bc45064fb121bf70e15cb16a5f8e8d7815
Everaldo Júnior <everaldocrj@gmail.com>
2023-03-13 17:02:41 -0300

    🎃 [ NF ] - [ Importação ]

--- app/Imports/AssociadosImport.php
+++ app/Imports/AssociadosImport.php
@@ -10,19 +10,79 @@ use App\Classes\Util;
 
 class AssociadosImport implements ToModel, WithHeadingRow, WithCalculatedFormulas
 {
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
-        $associado = new Associado;
-        
-        $associado->nome = $row["nome"];
-        $associado->cpf = $row["cpf"];
-        $associado->registro_profissional = $row["registro_profissional"];
+	/**
+	 * @param array $row
+	 *
+	 * @return \Illuminate\Database\Eloquent\Model|null
+	 */
+	public function model(array $row)
+	{
+		$associado = new Associado;
 
-        return $associado;
-    }
+		// dd($row);
+
+
+		// $associado->nome = $row["nome"];
+		// $associado->cpf = $row["cpf"];
+		// $associado->registro_profissional = $row["registro_profissional"];
+
+		if (
+			!array_key_exists('nome', $row) ||
+			!array_key_exists('cpf', $row) ||
+			!array_key_exists('rnp', $row) ||
+			!array_key_exists('email', $row) ||
+			!array_key_exists('telefone', $row) ||
+			!array_key_exists('endereco', $row) ||
+			!array_key_exists('cidade', $row) ||
+			!array_key_exists('uf', $row) ||
+			!array_key_exists('pais', $row) ||
+			!array_key_exists('inscrito_desde', $row) ||
+			!array_key_exists('nascimento', $row) ||
+			!array_key_exists('modalidade', $row) ||
+			!array_key_exists('conselho', $row) ||
+			!array_key_exists('titulo', $row) ||
+			!array_key_exists('situacao', $row)
+		) {
+			dd('Atenção: Planilha fora dos padrões.');
+			return;
+		}
+
+		// dd(date('Y-d-m H:i:s'));
+
+
+		if ($row['cpf'] == "" || $row['situacao'] == "") {
+			return;
+		}
+
+		$associado->nome = $row["nome"];
+		$associado->cpf = $row["cpf"];
+		$associado->registro_profissional = $row["rnp"];
+		$associado->email = $row["email"];
+		$associado->telefone = $row["telefone"];
+		$associado->endereco_atendimento = $row["endereco"];
+		$associado->cidade_atendimento = $row["cidade"];
+		$associado->uf_atendimento = $row["uf"];
+		$associado->cep_atendimento = $row["cep"];
+		$associado->pais_atendimento = $row["pais"];
+
+
+		if ($row['inscrito_desde'] !== "" && $row['inscrito_desde'] !== null) {
+			$associado->created_at = date('Y-m-d H:i:s', strtotime($row["inscrito_desde"]));
+		}
+
+		if ($row['nascimento'] !== "" && $row['nascimento'] !== null) {
+			$associado->nascimento = date('y-m-d', strtotime($row["nascimento"]));
+		}
+
+
+		$associado->modalidade = array_search($row["modalidade"], config("associados.modalidades"));
+		$associado->conselho_profissional = array_search($row["conselho"], config("associados.conselhos"));
+		$associado->titulo_profissional = array_search($row["titulo"], config("associados.titulis"));
+		$associado->situacao = array_search($row["situacao"], config("associados.situacoes"));
+
+
+
+
+		return $associado;
+	}
 }
--- resources/views/exports/associados.blade.php
+++ resources/views/exports/associados.blade.php
@@ -1,21 +1,22 @@
 <table>
-    <thead>
+    {{-- <thead>
         <tr>
             <td colspan="2"><b>Relação de Associados</b></td>
         </tr>
-    </thead>
+    </thead> --}}
     <tbody>
-        <tr>
+        {{-- <tr>
             <td></td>
             <td></td>
             <td></td>
             <td></td>
             <td></td>
-        </tr>
+        </tr> --}}
         <tr>
             {{-- <td>FOTO</td> --}}
             <td>SEQ.</td>
             <td>NOME</td>
+            <td>INSCRITO DESDE</td>
             <td>MODALIDADE</td>
             <td>TITULO</td>
             <td>CPF</td>
@@ -29,27 +30,42 @@
             <td>UF</td>
             <td>CEP</td>
             <td>PAIS</td>
-            <td>SITUAÇÃO</td>
+            <td>SITUACAO</td>
         </tr>
-        @foreach($associados as $associado)
+        @foreach ($associados as $associado)
             <tr>
                 {{-- <td><img src="{{ $associado->foto }}" alt=""></td> --}}
                 <td>{{ $associado->id }}</td>
                 <td>{{ $associado->nome }}</td>
-                <td>@if($associado->modalidade !== null) {{ config("associados.modalidades")[$associado->modalidade] }} @endif</td>
-                <td>@if($associado->titulo_profissional !== null) {{ config("associados.titulis")[$associado->titulo_profissional] }} @endif</td>
+                <td>
+                    {{ date('d-m-Y G:i:s', strtotime($associado->created_at)) }}
+                </td>
+                <td>
+                    @if ($associado->modalidade !== null)
+                        {{ config('associados.modalidades')[$associado->modalidade] }}
+                    @endif
+                </td>
+                <td>
+                    @if ($associado->titulo_profissional !== null)
+                        {{ config('associados.titulis')[$associado->titulo_profissional] }}
+                    @endif
+                </td>
                 <td>{{ $associado->cpf }}</td>
                 <td>{{ $associado->registro_profissional }}</td>
-                <td>@if($associado->nascimento) {{ date("d/m/Y", strtotime($associado->nascimento)) }} @endif</td>
+                <td>
+                    @if ($associado->nascimento)
+                        {{ date('d-m-Y', strtotime($associado->nascimento)) }}
+                    @endif
+                </td>
                 <td>{{ $associado->email }}</td>
                 <td>{{ $associado->telefone }}</td>
-                <td>{{ config("associados.conselhos")[$associado->conselho_profissional] }}</td>
+                <td>{{ config('associados.conselhos')[$associado->conselho_profissional] }}</td>
                 <td>{{ $associado->endereco_atendimento }}</td>
                 <td>{{ $associado->cidade_atendimento }}</td>
                 <td>{{ $associado->uf_atendimento }}</td>
                 <td>{{ $associado->cep_atendimento }}</td>
                 <td>{{ $associado->pais_atendimento }}</td>
-                <td>{{ config("associados.situacoes")[$associado->situacao] }}</td>
+                <td>{{ config('associados.situacoes')[$associado->situacao] }}</td>
             </tr>
         @endforeach
     </tbody>
